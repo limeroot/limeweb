a8d6f985d9b91b2e8e1399e91b4902265457bc60
zerolinux <carlosrincon2005@gmail.com>
2013-05-17 20:32:02 -0500

    php file that generate json data as demo for alerts bubble in limeweb

---
+++ alerts.php
@@ -0,0 +1,7 @@
+<?php
+    $data[0] = array("title" => "ISP1", "alert" => "Connection lost", "date" => "Apr 01, 2013@21:28", "origin" => "LiMon");
+    $data[1] = array("title" => "HDD", "alert" => "Temperature over 40ºC", "date" => "Apr 01, 2013@22:14", "origin" => "LimeCare");
+    $data[2] = array("title" => "ISP1", "alert" => "Connection lost", "date" => "Apr 01, 2013@21:28", "origin" => "LiMon");
+    $data[3] = array("title" => "ISP1", "alert" => "Connection lost", "date" => "Apr 01, 2013@21:28", "origin" => "LiMon");
+    echo json_encode($data);
+?>
